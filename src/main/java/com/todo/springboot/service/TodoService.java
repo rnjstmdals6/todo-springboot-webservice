cd3a735ea751ae9c3eb296a89381edda11c615d5
rnjstmdals6 <rnjstmdals6@naver.com>
2022-02-08 23:37:55 +0900

    Added TestService

---
+++ src/main/java/com/todo/springboot/service/TodoService.java
@@ -0,0 +1,11 @@
+package com.todo.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    public String testService(){
+        return "Test Service";
+    }
+}
