7b803495baa63a4e3fd3ed37943897ba065c2a51
rnjstmdals6 <rnjstmdals6@naver.com>
2022-02-08 23:56:32 +0900

    Added TodoRepository.java

---
+++ src/main/java/com/todo/springboot/persistence/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.todo.springboot.persistence;
+
+import com.todo.springboot.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+}
