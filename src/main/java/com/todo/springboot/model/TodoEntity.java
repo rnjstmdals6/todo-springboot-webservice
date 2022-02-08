9d96f1c771d3b6836d5b32b68e7cd77445094559
rnjstmdals6 <rnjstmdals6@naver.com>
2022-02-08 23:56:55 +0900

    Modified TodoEntity.java

--- src/main/java/com/todo/springboot/model/TodoEntity.java
+++ src/main/java/com/todo/springboot/model/TodoEntity.java
@@ -4,12 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "Todo")
 public class TodoEntity {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
     private String id; // 오브젝트 아이디
     private String userId; // 오브젝트 생성한 사용자의 아이디
     private String title; // ex) 운동하기
