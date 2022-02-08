22a5b2c5ecac0c11bd9d34f2b4f8132e36ab33d7
rnjstmdals6 <rnjstmdals6@naver.com>
2022-02-09 00:31:03 +0900

    Modified TodoRepository.java

--- src/main/java/com/todo/springboot/persistence/TodoRepository.java
+++ src/main/java/com/todo/springboot/persistence/TodoRepository.java
@@ -2,8 +2,13 @@ package com.todo.springboot.persistence;
 
 import com.todo.springboot.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+
+    List<TodoEntity> findByUserId(String userId);
 }
